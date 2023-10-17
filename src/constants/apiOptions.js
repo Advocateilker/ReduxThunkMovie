4dadc6ef70b5331192c6de92869d9ca234c8d1db
Mustafa İlker Tekir <112612495+Advocateilker@users.noreply.github.com>
2023-10-17 13:24:08 +0300

    Update apiOptions.js
    Api Key Hidden

--- src/constants/apiOptions.js
+++ src/constants/apiOptions.js
@@ -3,7 +3,7 @@ export const popularOptions = {
     params: {language: 'en-US', page: '4'},
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTc3N2IzMGQ1ZGFjMDg4NTBhZWViMDY1OTA2NjhhMSIsInN1YiI6IjY1MmQwNzZlNzJjMTNlMDEwMzUyNTUwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PDopAw018CMuLLxc45Oz7wn0sBmhQK0vNO4frukNkyw'
+      Authorization: 'SECRET API KEY'
     }
   };
 
